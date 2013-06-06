e49cc7f1bf61d8b8f2f486926802ff65b66e5432
CuleX <cculex@gmail.com>
2013-06-06 19:50:57 +0200

    Convert tabs to spaces since it was a mix.
    Also gitignore for my binary output name.

--- universalbrsarpatcher.cpp
+++ universalbrsarpatcher.cpp
@@ -37,8 +37,8 @@ int main(int argc, const char* argv[])
         file_content.reserve(file_size);
         char buffer[16384];
         std::streamsize chars_read;
-		int brspos = 0;
-		
+        int brspos = 0;
+        
         while (file.read(buffer, sizeof buffer), chars_read = file.gcount())
             file_content.append(buffer, chars_read);
 
@@ -49,62 +49,62 @@ int main(int argc, const char* argv[])
                  (found_at = file_content.find(search_term, offset)) !=
                                                             std::string::npos;
                  offset = found_at + search_term_size)
-				 brspos = found_at - 28; 
+                 brspos = found_at - 28; 
                  std::cout << brspos << std::endl;
-				 std::cout << std::hex << brspos << std::endl;
+                 std::cout << std::hex << brspos << std::endl;
         }
-		
-		FILE * brstm;
-//		FILE * brsar;
-		std::ofstream brsar;
-		char brstmbuffer [33];
-		brstm = fopen ( search_term, "rb" );
-		brsar.open( filename, ios::in|ios::out|ios::binary);
-		//fputs ( "This is an apple." , pFile );
-		fseek ( brstm , 0 , SEEK_END );
-		brsar.seekp(brspos);
-		std::string brstmsize = ""; 
-		brstmsize.insert(0,itoa(ftell(brstm),brstmbuffer,16));		
-		while ( brstmsize.length() < 8 )
-			brstmsize.insert(0,"0");
-		int i;
-		int temp;
-		unsigned char rbytes[4];
-		for( i = 0; i < 4; ++i ) {
+        
+        FILE * brstm;
+//        FILE * brsar;
+        std::ofstream brsar;
+        char brstmbuffer [33];
+        brstm = fopen ( search_term, "rb" );
+        brsar.open( filename, ios::in|ios::out|ios::binary);
+        //fputs ( "This is an apple." , pFile );
+        fseek ( brstm , 0 , SEEK_END );
+        brsar.seekp(brspos);
+        std::string brstmsize = ""; 
+        brstmsize.insert(0,itoa(ftell(brstm),brstmbuffer,16));        
+        while ( brstmsize.length() < 8 )
+            brstmsize.insert(0,"0");
+        int i;
+        int temp;
+        unsigned char rbytes[4];
+        for( i = 0; i < 4; ++i ) {
         sscanf( brstmsize.c_str() + 2 * i, "%2x", &temp );
         rbytes[i] = temp;
-		}	
-		brsar.write((char*)rbytes, 4);
-//		fwrite(rbytes, 1, sizeof(rbytes), brsar);
-		std::cout << rbytes << std::endl;
-//		std::cout << search_term + " patched in" << std::endl;
-//		fputs ( " sam" , pFile );
-		brsar.close();
-		fclose ( brstm );
+        }    
+        brsar.write((char*)rbytes, 4);
+//        fwrite(rbytes, 1, sizeof(rbytes), brsar);
+        std::cout << rbytes << std::endl;
+//        std::cout << search_term + " patched in" << std::endl;
+//        fputs ( " sam" , pFile );
+        brsar.close();
+        fclose ( brstm );
     }
 }
 
 char* itoa(int value, char* result, int base) {
-		// check that the base if valid
-		if (base < 2 || base > 36) { *result = '\0'; return result; }
-	
-		char* ptr = result, *ptr1 = result, tmp_char;
-		int tmp_value;
-	
-		do {
-			tmp_value = value;
-			value /= base;
-			*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-		} while ( value );
-	
-		// Apply negative sign
-		if (tmp_value < 0) *ptr++ = '-';
-		*ptr-- = '\0';
-		while(ptr1 < ptr) {
-			tmp_char = *ptr;
-			*ptr--= *ptr1;
-			*ptr1++ = tmp_char;
-		}
-		return result;
+        // check that the base if valid
+        if (base < 2 || base > 36) { *result = '\0'; return result; }
+    
+        char* ptr = result, *ptr1 = result, tmp_char;
+        int tmp_value;
+    
+        do {
+            tmp_value = value;
+            value /= base;
+            *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+        } while ( value );
+    
+        // Apply negative sign
+        if (tmp_value < 0) *ptr++ = '-';
+        *ptr-- = '\0';
+        while(ptr1 < ptr) {
+            tmp_char = *ptr;
+            *ptr--= *ptr1;
+            *ptr1++ = tmp_char;
+        }
+        return result;
 }
 
